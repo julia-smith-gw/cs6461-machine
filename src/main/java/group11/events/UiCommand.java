32c573bf1bf0af60e1b8f49867c7c313ba799fc7
smithj11 <julia.smith@wustl.edu>
2025-10-20 19:56:29 -0400

    Added new ui fields, new events, files for cache

--- src/main/java/group11/events/UiCommand.java
+++ src/main/java/group11/events/UiCommand.java
@@ -2,7 +2,7 @@ package group11.events;
 
 
 // UI -> Model (intents/commands)
-public sealed interface UiCommand permits SetGPR, SetIXR, SetPC, SetMAR, SetMBR {}
+public sealed interface UiCommand permits SetGPR, SetIXR, SetPC, SetMAR, SetMBR, SetConsoleInput {}
 
 
 
